2f703e92bc0ae6bb25f87c01c51176fdfd838d1c
Caio <c410.f3r@gmail.com>
2025-02-04 00:40:34 -0300

    Add ohkami

---
+++ protocols/web-socket/ohkami/src/main.rs
@@ -0,0 +1,15 @@
+use ohkami::prelude::*;
+use ohkami::ws::{WebSocketContext, WebSocket, Message};
+
+async fn echo_text(ctx: WebSocketContext<'_>) -> WebSocket {
+    ctx.upgrade(|mut conn| async move {
+        while let Ok(Some(Message::Text(text))) = conn.recv().await {
+            conn.send(text).await.expect("failed to send text");
+        }
+    })
+}
+
+#[tokio::main]
+async fn main() {
+    Ohkami::new(("/".GET(echo_text))).howl("0.0.0.0:9000").await
+}
